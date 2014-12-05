5f97382832bb6e10df25878dfb6a305fdb914b8b
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2014-12-05 17:10:22 +0100

    added datetime and user to EProject

--- learninghub/learningbucket/models.py
+++ learninghub/learningbucket/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 """
 Models for learning bucket
 @author Per-Henrik Kvalnes
 """
 
-
+# E-learning project
 class EProject(models.Model):
-    # E-learning project
+
     name = models.CharField(max_length=40)
+    timecreated = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, User)
     
 
