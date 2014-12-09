8206a7685773ad89cb4bf54bd2d84822e7733a06
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2014-12-09 20:52:51 +0100

    search posible (with limmited capabilities)

--- learninghub/searchbucket/views.py
+++ learninghub/searchbucket/views.py
@@ -12,7 +12,14 @@ import learningbucket.models as models
 
 def searchProjects(request):
     
-    c = {}
+    c = {"results":False}
+
+    if 'tags' in request.GET:
+        tags_get = request.GET['tags']
+        tags = models.EProjectTag.objects.all().filter(tag=tags_get)
+        c['tags'] = tags
+        c['results'] = True
+
     template = loader.get_template("search_project.html")
-    context = RequestContext(request, c)
+    context = RequestContext(request, c) 
     return HttpResponse(template.render(context))
