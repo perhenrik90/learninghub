00f4bb7a66d2dfe329464836bfb302ca7ee563f7
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2014-12-14 14:40:20 +0100

    tested with mysql

--- learninghub/learninghub/settings.py
+++ learninghub/learninghub/settings.py
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'learninghub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'learninghub',
+        'USER':'learninghub',
+        'PASSWORD':'1234',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
     }
 }
 
