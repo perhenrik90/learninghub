5a46caad52f9927703f7a605093f9bad8bd81a7f
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2014-12-18 08:43:26 +0100

    added a subdomainprefix on the page

--- learninghub/learninghub/urls.py
+++ learninghub/learninghub/urls.py
@@ -5,44 +5,46 @@ from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
+up = "lh/" # prefix
+
 urlpatterns = patterns('',
     # learning bucket app/module
-    url(r'^$', 'learningbucket.views.login_view',name='Login'),
-    url(r'^login/$', 'learningbucket.views.login_view', name='Login'),
-    url(r'^logout/$', 'learningbucket.views.logout_view', name='Logout'),
-    url(r'^error/$', 'learningbucket.views.error_view', name='Error'),
+    url(r'^'+up+'$', 'learningbucket.views.login_view',name='Login'),
+    url(r'^'+up+'login/$', 'learningbucket.views.login_view', name='Login'),
+    url(r'^'+up+'logout/$', 'learningbucket.views.logout_view', name='Logout'),
+    url(r'^'+up+'error/$', 'learningbucket.views.error_view', name='Error'),
 
-    url(r'^myprojects/$', 'learningbucket.views.myprojects', name='My Projects'),
-    url(r'^createproject/$', 'learningbucket.views.createproject', name='Create project'),
-    url(r'^project/$', 'learningbucket.views.project', name='Project'),
-    url(r'^project/upload$', 
+    url(r'^'+up+'myprojects/$', 'learningbucket.views.myprojects', name='My Projects'),
+    url(r'^'+up+'createproject/$', 'learningbucket.views.createproject', name='Create project'),
+    url(r'^'+up+'project/$', 'learningbucket.views.project', name='Project'),
+    url(r'^'+up+'project/upload$', 
         'learningbucket.views.project_upload_file', 
         name='Upload'),
-    url(r'^project/deletefile$', 
+    url(r'^'+up+'project/deletefile$', 
         'learningbucket.views.deleteFile', 
         name='Upload'),
-    url(r'^project/edit$', 
+    url(r'^'+up+'project/edit$', 
         'learningbucket.views.editproject', 
         name='Edit'),
-    url(r'^project/download$', 
+    url(r'^'+up+'project/download$', 
         'learningbucket.views.project_download_file', 
         name='Upload'),
-    url(r'^project/postcomment$', 
+    url(r'^'+up+'project/postcomment$', 
         'learningbucket.views.project_post_comment', 
         name='Upload'),
 
     # search bucket app/module 
-    url(r'^search$','searchbucket.views.searchProjects',
+    url(r'^'+up+'search$','searchbucket.views.searchProjects',
         name='Search'),
 
     # profile bucket app/module
-    url(r'^profile$','profilebucket.views.profile',
+    url(r'^'+up+'profile$','profilebucket.views.profile',
         name='Profile'),
-    url(r'^profilemenu$','profilebucket.views.profile_menu',
+    url(r'^'+up+'profilemenu$','profilebucket.views.profile_menu',
         name='Profile Menu'),
-    url(r'^profilepassword$','profilebucket.views.profile_change_password',
+    url(r'^'+up+'profilepassword$','profilebucket.views.profile_change_password',
         name='Profile Menu'),
     
     # admin app/module
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^'+up+'admin/', include(admin.site.urls)),
 )
